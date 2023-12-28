2c5b433e17dd7dd31628c1ec048d2643861da8cd
Huynh <huynhkin269@gmail.com>
2023-12-28 19:13:43 +0700

    hhh

--- Project3/Project3/obj/Debug/net6.0/Project3.AssemblyInfo.cs
+++ Project3/Project3/obj/Debug/net6.0/Project3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8783afafb6189c2cdcf9846efdf662191ce9967")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a834711ef564f9a4256703f86d51ee59765b4b1f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
